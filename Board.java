2f1c4290cc9f47ce4fe4d476ef1b116c4a881573
MaGa <guser1996.gu@gmail.com>
2015-10-27 20:23:43 +0100

    Initial commit contains: unmodified Pentomino, Board, MatrixHandler, SolvePents copied from previous project added empty .java files: Game, HScore, Player, Bot, TetrisGui

---
+++ Board.java
@@ -0,0 +1,416 @@
+import java.util.*;
+
+/** We ll still stick to columns first and rows second for the counting */
+public class Board {
+	
+	public static Board assembleAuto (Board part1, Board part2, Board original)
+	{
+		if (original.getWidth() - part1.getWidth() - part2.getWidth() >= 0 && original.getWidth() - part1.getWidth() - part2.getWidth() <= 1)
+			return assembleBoardsW (part1, part2, original.getWidth());
+		else if (original.getHeight() - part1.getHeight() - part2.getHeight() >= 0 && original.getHeight() - part1.getHeight() - part2.getHeight() <= 1)
+			return assembleBoardsH (part1, part2, original.getHeight());
+		return new Board (0, 0);
+	}
+	
+	/**puts two seperate Boards together, on top of each other => along the width*/
+	public static Board assembleBoardsW (Board part1, Board part2, int desiredW)
+	{
+		//if added widths == desiredW (+1 optional) and heights are equal
+		if (desiredW - part1.getWidth() - part2.getWidth() >= 0 && desiredW - part1.getWidth() - part2.getWidth() <= 1 && part1.getHeight() == part2.getHeight())
+		{
+			int startPart2 = desiredW - part2.getWidth();
+			//matrix of assembled board
+			MatrixHandler newBoardMatrix = new MatrixHandler (desiredW, part1.getHeight());
+			//generate clone from part2 to ensure that no values outside method call are changed
+			part2 = part2.clone();
+			//rotates part2 by 180 degrees
+			part2.rotate();
+			part2.rotate();
+			//copies values from parts to new matrix
+			for (int cCol = 0; cCol < desiredW; cCol++)
+			{
+				for (int cRow = 0; cRow < part1.getHeight(); cRow++)
+				{
+					if (cCol < part1.getWidth())
+						newBoardMatrix.setCell (cCol, cRow, part1.getElement (cCol, cRow));
+					else if (cCol >= startPart2)
+						newBoardMatrix.setCell (cCol, cRow, part2.getElement (cCol - startPart2, cRow));
+				}
+			}
+			//returns assembled board on success
+			return new Board (newBoardMatrix);			
+		}
+		//returns crap if failure
+		return new Board (0, 0);
+	}
+	/**puts two seperate Boards together, side by side => along the height*/
+	public static Board assembleBoardsH (Board part1, Board part2, int desiredH)
+	{
+		if (desiredH - part1.getHeight() - part2.getHeight() >= 0 && desiredH - part1.getHeight() - part2.getHeight() <= 1 && part1.getWidth() == part2.getWidth())
+		{
+			int startPart2 = desiredH - part2.getHeight();
+			MatrixHandler newBoardMatrix = new MatrixHandler (part1.getWidth(), desiredH);
+			part2 = part2.clone();
+			part2.rotate();
+			part2.rotate();
+			for (int cCol = 0; cCol < part1.getWidth(); cCol++)
+			{
+				for (int cRow = 0; cRow < desiredH; cRow++)
+				{
+					if (cRow < part1.getHeight())
+						newBoardMatrix.setCell (cCol, cRow, part1.getElement (cCol, cRow));
+					else if (cRow >= startPart2)
+						newBoardMatrix.setCell (cCol, cRow, part2.getElement (cCol, cRow - startPart2));
+				}
+			}
+			return new Board (newBoardMatrix);			
+		}
+		
+		return new Board (0, 0);
+	}
+	
+	
+	/**checks if pentominoes can be placed on board given the size*/
+	public static boolean sizeCheck(int w, int h){
+		return (h >= 3 && w >= 3 && (h * w == 60));
+	}
+	
+	/** The constant number of Pentominos */	
+	public static final int mNO_PENTS = 12;
+	/** The constant number of cells in the board */
+	public static final int mAREA = 60;
+			
+	
+	/** Takes width and height and creates an object of type MatrixHandler and the arraylist of Pentominos */
+	public Board(int w, int h) {
+		mMatrix = new MatrixHandler(w, h);
+		mListPents = new ArrayList<Pentomino>();
+	}
+	
+	/** Takes an array of type MatrixHandler, clones it and creates the arraylist of Pentominos */
+	public Board(MatrixHandler array) {
+		mMatrix = array.clone();
+		mListPents = new ArrayList<Pentomino>();
+	}
+	
+	/** Takes the board, clones it and creates the arraylist of Pentominos */
+	public Board(Board copyBoard) {
+		mMatrix = copyBoard.mMatrix.clone();
+		mListPents = new ArrayList<Pentomino>();
+		
+		for (int i=0; i < copyBoard.mListPents.size(); i++) {
+			mListPents.add(copyBoard.mListPents.get(i));
+		}
+	}
+	
+	/** Returns the width of mMatrix */
+	public int getWidth() {
+		return mMatrix.getWidth();
+	}
+	
+	/** Returns the height of mMatrix */
+	public int getHeight() {
+		return mMatrix.getHeight();
+	}
+	
+	/** This method checks if there are empty cells left on the Board and returns false if that's the case */
+	public boolean isFilled() {
+		for (int i=0; i<this.getWidth(); i++) {
+			for (int j=0; j<this.getHeight(); j++) {
+				//changed mMatrix.getElement() to this.getElement()
+				if (this.getElement(i,j) == 0) return false;
+			}
+		}
+		return true;
+	}
+	/** This method puts the Pentomino on the LeftTop and returns true if succeeded */
+	public boolean putLeftTop(Pentomino pent) {
+		int position = this.getLeftTop();
+		int x = getXCoor(position);
+		int y = getYCoor(position);
+		int xpent = pent.getXCoor(pent.getLeftTop());
+		int ypent = pent.getYCoor(pent.getLeftTop());
+		//System.out.println("Pent.getlefttop: " + pent.getLeftTop());
+		int newX = x - xpent;
+		int newY = y - ypent;
+		//System.out.println("X: " + x + " Y: " + y );
+		//System.out.println(pent.getLeftTop());
+		//System.out.println("newX: " + newX + " newY: " + newY);
+		if (newX < 0 || newY < 0) 
+		{
+			//System.out.println ("Failed adjusted coordinate check");
+			return false;
+		}
+		
+		if (this.pentFits(pent, position)) {
+			//System.out.println("Pent is now being placed.");
+			for (int i=0; i< pent.getWidth(); i++) {
+				for (int j=0; j< pent.getHeight(); j++) {
+					if (pent.getElement(i,j) != 0) {
+						mMatrix.setCell(i + newX, j + newY, pent.getElement(i,j));
+					}
+				}
+			}
+			mListPents.add (pent.clone());
+			return isSolvable(position, pent.getWidth(), pent.getHeight());
+		}
+		//System.out.println ("Failed fit check!");
+		return false;
+	}
+	
+	/**returns wether there is at least one pentomino placed on both boards*/
+	public boolean pentsPlacedIntersect (Board compare)
+	{
+		for (Pentomino iThis : this.mListPents)
+		{
+			for (Pentomino iCompare : compare.mListPents)
+			{
+				if (iThis.getId() == iCompare.getId())
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	/** This method counts the empty cells and returns whether there is a blocked empty cell */
+	public boolean isSolvable(int position, int lastPentW, int lastPentH) {
+		int x = getXCoor(position);
+		int y = getYCoor(position);
+		//Checks if region is at the boundaries
+		boolean rightBorder = false;
+		boolean bottomBorder = false;
+		if (x + lastPentW == this.getWidth()) rightBorder = true;
+		if (y + lastPentH == this.getHeight()) bottomBorder = true;
+		if (x>0) {
+			x--;
+			lastPentW++;
+		}
+		if (y>0) {
+			y--;
+			lastPentH++;
+		}
+		MatrixHandler region = mMatrix.createSubRegion(x, y, lastPentW, lastPentH);
+		for (int i=0; i<lastPentW; i++) {
+			for (int j=0; j<lastPentH; j++) {
+				if (region.getCell(i, j) == 0) 
+					if (!checkFill(region, i, j, rightBorder, bottomBorder)) 
+					{
+						//System.out.println ("Solvable check fails at " + (x + i) + "|" + (y + j));
+						return false;
+					}
+			}
+		}
+		return true;
+	}
+	/** Checks recursively the region around the LeftTop empty cell if there are any 'bridges' to the empty cells on the right side of the board. */
+	public boolean checkFill(MatrixHandler region, int x, int y, boolean rightB, boolean bottomB) {
+		//System.out.println ("Recursive check at " + x + "|" + y);
+		if ((x == region.getWidth() - 1 && !rightB) || (y == region.getHeight() - 1 && !bottomB))
+		{
+			if (region.getCell (x, y) == 0 || region.getCell (x, y) == mNO_PENTS + 1) {
+				region.setCell(x, y, mNO_PENTS +2);
+				return true;
+			}
+			else if (region.getCell (x, y) == mNO_PENTS + 2)
+				return true;
+		}
+		
+		/** Sets the value of the cell that is currently checked to 13 so that the recursion knows that this cell has already been checked */
+		region.setCell(x, y, mNO_PENTS + 1);
+		
+		for (int moveX = -1; moveX <= 1; moveX +=2)
+		{		
+			int newX = x + moveX;
+			if (region.getCell(newX, y) == 0) {
+				if (checkFill(region, newX, y, rightB, bottomB)) {
+					region.setCell(newX, y, mNO_PENTS +2);
+					return true;
+				}
+			}
+			/** Returns true if cell has already been checked positively */
+			if (region.getCell(newX, y) == mNO_PENTS+2) return true;
+		}
+		for (int moveY = -1; moveY <= 1; moveY += 2)
+		{
+			int newY = y + moveY;
+			if (region.getCell(x, newY) == 0)
+			{
+				if (checkFill(region, x, newY, rightB, bottomB)) {
+					region.setCell(x, newY, mNO_PENTS +2);
+					return true;
+				}
+			}
+			/** Returns true if cell has already been checked positively */
+			if (region.getCell(x, newY) == mNO_PENTS+2) return true;
+		}
+		return false;
+	}
+	
+	
+	/** Checks if the pentomino fits into LeftTop and returns boolean */
+	public boolean pentFits(Pentomino pent, int position) {	
+		//System.out.println ("entering pentfits");
+		//Gets X and Y coordinates from the exact position
+		int x = getXCoor(position);
+		int y = getYCoor(position);
+		int xpent = -1;
+		int ypent = -1;
+		/** newX and newY are adjusted to the first filled cell of the pentomino */
+		//Gets X and Y coordinates of Pentomino
+		if (mMatrix.getCell(x, y) == 0) {
+			xpent = pent.getXCoor(pent.getLeftTop());
+			ypent = pent.getYCoor(pent.getLeftTop());
+			//System.out.println("XPENT: " + xpent + " YPENT: " + ypent);
+			//System.out.println("Width pent: " + pent.getWidth() + " Height pent: " + pent.getHeight());
+		} else {
+			//System.out.println ("Topleft cell " + x + "|" + y + " given is not empty");
+			return false;
+		}
+		int newX = x - xpent;
+		int newY = y - ypent;
+		//System.out.println ("Got X = " + x + " y = " + y);
+		//System.out.println("NewX: " + newX + " NewY: " + newY);
+		/** Checks if pent is in boundaries */
+		if (((newX+pent.getWidth()) > this.getWidth()) || ((newY+pent.getHeight()) > this.getHeight())) 
+		{
+			//System.out.println ("Pentomino matrix does not fit in board");
+			return false;
+		}
+		
+		/** Loops through pent and Board and checks if Board==0||Pent==0 */
+		for (int i=0; i< pent.getWidth(); i++) {
+			for (int j=0; j< pent.getHeight(); j++) {
+				if (this.getElement(newX + i, newY + j) != 0 && pent.getElement(i, j) != 0) 
+				{
+					//System.out.println ("Non matching element at " + i + "|" + j + " on pentomino and at " + 
+						//(newX + i) + "|" + (newY + j) + " on board");
+					return false;
+				}
+			}
+		}
+		return true;				
+	}
+	
+	public boolean equals (Board compare)
+	{
+		return (this.mMatrix.equals (compare.mMatrix));	
+	}
+	
+	public boolean isEmpty ()
+	{
+		for (int cCol = 0; cCol < this.getWidth(); cCol++)
+		{
+			for (int cRow = 0; cRow < this.getHeight(); cRow++)
+			{
+				if (this.getElement (cCol, cRow) != 0)
+					return false;
+			}
+		}
+		return true;
+	}
+			
+	/** Returns the X position of the cell */
+	public int getXCoor(int position) {
+		int height = mMatrix.getHeight();
+		int positionX = (int)Math.floor((position-1)/height);
+		return positionX;
+	}
+	
+	/** Returns the Y position of the cell */
+	public int getYCoor(int position) {
+		int height = mMatrix.getHeight();
+		int positionY = (int)((position-1)%height);
+		return positionY;
+	}
+		
+	/** Takes the coordinates x and y and returns the value of that cell */
+	public int getElement(int x, int y) {
+		return mMatrix.getCell(x, y);
+	}
+	
+	/** Returns a clone of mMatrix */
+	public Board clone() {
+		Board cloneBoard = new Board(mMatrix);
+		return cloneBoard;		
+	}
+	
+	/**determines best way to split uniformly boards*/ 
+	public Board splitHalfFloor (int pentSquares)
+	{
+		//if width is larger than height and maximum-sized pentomino could still be placed inside the splitted Board
+		if (this.getWidth() >= this.getHeight() && (int)Math.floor (this.getWidth() / 2) >= pentSquares)
+			return new Board ((int)Math.floor (this.getWidth() / 2), this.getHeight());
+		//opposite of previous case
+		else if (this.getHeight() > this.getWidth() && (int)Math.floor (this.getHeight() / 2) >= pentSquares)
+			return new Board (this.getWidth(), (int)Math.floor (this.getHeight() / 2));
+		//if splitted Board would prevent program from finding all solutions
+		return new Board (0, 0);
+	}
+	
+	/**returns pentominos not used*/
+	public ArrayList <Pentomino> unusedPents (ArrayList <Pentomino> totalPents)
+	{
+		ArrayList <Pentomino> unused = new ArrayList <Pentomino>();
+		for (Pentomino check : totalPents)
+		{
+			int iPlaced = 0;
+			while (iPlaced < mListPents.size() && mListPents.get (iPlaced).getId() != check.getId())
+				++iPlaced;
+			if (iPlaced >= mListPents.size())
+				unused.add (check.clone());
+		}
+		return unused;
+	}
+	
+	public void rotate()
+	{
+		mMatrix.rotate();	
+	}
+	
+	public void flipVer()
+	{
+		mMatrix.flipVer();	
+	}
+	
+	public void flipHor()
+	{
+		mMatrix.flipHor();	
+	}
+	
+	public void printBoard() {
+		System.out.println("Height: " + this.getHeight());
+		System.out.println("Width: " + this.getWidth());
+		for (int i=0; i<this.getHeight(); i++) {
+			for (int j=0; j<this.getWidth(); j++) {
+				System.out.print(mMatrix.getCell(j,i) + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	
+	//Private 
+//CHANGE
+	/** This method gets the LeftTop cell and returns the position as an integer */
+	public int getLeftTop() {
+		int count = 0;
+		for (int i=0; i<this.getWidth(); i++) {
+			for (int j=0; j<this.getHeight(); j++) {
+				count++;
+				if (this.getElement(i,j) == 0) return count;
+			}
+		}
+		return -1;
+	}
+	
+	
+		
+		
+	
+
+	/** Matrix of type MatrixHandler */
+	private MatrixHandler mMatrix;
+	
+	/** Arraylist of type Pentomino */
+	private ArrayList<Pentomino> mListPents;
+}
---
+++ Pentomino.java
@@ -0,0 +1,297 @@
+import java.util.ArrayList;
+
+public class Pentomino { 
+
+	/**Arraylist with the 12 original pentominoes */
+	public static ArrayList<Pentomino> createsPentList() {
+
+	ArrayList<Pentomino> pentList = new ArrayList<Pentomino>();
+	int[][][] grid1 = new int[6][3][3];
+	int[][][] grid2 = new int[2][2][3];
+	int[][][] grid3 = new int[3][2][4];
+	int[][][] grid4 = new int[1][1][5];
+		
+	//Create P1
+		for (int j=0; j<grid2[0].length; j++) {
+			for (int k=0; k<grid2[0][j].length; k++) {
+				grid2[0][j][k] = 1;
+			}
+		}
+		grid2[0][1][2] = 0;
+		
+		
+		//Create X
+		for (int j=0; j<grid1[0].length; j++) {
+			for (int k=0; k<grid1[0][j].length; k++) {
+				grid1[0][j][k] = 2;
+			}
+		}
+		grid1[0][0][0] = 0;
+		grid1[0][0][2] = 0;
+		grid1[0][2][0] = 0;
+		grid1[0][2][2] = 0;
+		
+		//Create F1
+		for (int j=0; j<grid1[0].length; j++) {
+			for (int k=0; k<grid1[0][j].length; k++) {
+				grid1[1][j][k] = 3;
+			}
+		}
+		grid1[1][0][0] = 0;
+		grid1[1][0][2] = 0;
+		grid1[1][2][1] = 0;
+		grid1[1][2][2] = 0;
+		
+		//Create V1
+		for (int j=0; j<grid1[0].length; j++) {
+			for (int k=0; k<grid1[0][j].length; k++) {
+				grid1[2][j][k] = 4;
+			}
+		}
+		grid1[2][1][0] = 0;
+		grid1[2][1][1] = 0;
+		grid1[2][2][0] = 0;
+		grid1[2][2][1] = 0;
+		
+		//Create W1
+		for (int j=0; j<grid1[0].length; j++) {
+			for (int k=0; k<grid1[0][j].length; k++) {
+				grid1[3][j][k] = 5;
+			}
+		}
+		grid1[3][0][2] = 0;
+		grid1[3][1][0] = 0;
+		grid1[3][2][0] = 0;
+		grid1[3][2][1] = 0;
+		
+		//Create Y1
+		for (int j=0; j<grid3[0].length; j++) {
+			for (int k=0; k<grid3[0][j].length; k++) {
+				grid3[0][j][k] = 6;
+			}
+		}
+		grid3[0][0][0] = 0;
+		grid3[0][0][2] = 0;
+		grid3[0][0][3] = 0;
+		
+		//Create I1
+		for (int k=0; k<grid4[0][0].length; k++) {
+			grid4[0][0][k] = 7;
+			}
+		
+		
+		//Create T1
+		for (int j=0; j<grid1[0].length; j++) {
+			for (int k=0; k<grid1[0][j].length; k++) {
+				grid1[4][j][k] = 8;
+			}
+		}
+		grid1[4][0][1] = 0;
+		grid1[4][0][2] = 0;
+		grid1[4][2][1] = 0;
+		grid1[4][2][2] = 0;
+		
+		//Create Z1
+		for (int j=0; j<grid1[0].length; j++) {
+			for (int k=0; k<grid1[0][j].length; k++) {
+				grid1[5][j][k] = 9;
+			}
+		}
+		grid1[5][0][1] = 0;
+		grid1[5][0][2] = 0;
+		grid1[5][2][0] = 0;
+		grid1[5][2][1] = 0;
+		
+		//Create U1
+		for (int j=0; j<grid2[0].length; j++) {
+			for (int k=0; k<grid2[0][j].length; k++) {
+				grid2[1][j][k] = 10;
+			}
+		}
+		grid2[1][1][1] = 0;
+		
+		//Create N1
+		for (int j=0; j<grid3[0].length; j++) {
+			for (int k=0; k<grid3[0][j].length; k++) {
+				grid3[1][j][k] = 11;
+			}
+		}
+		grid3[1][0][0] = 0;
+		grid3[1][1][2] = 0;
+		grid3[1][1][3] = 0;
+		
+		//Create L1
+		for (int j=0; j<grid3[0].length; j++) {
+			for (int k=0; k<grid3[0][j].length; k++) {
+				grid3[2][j][k] = 12;
+			}
+		}
+		grid3[2][1][0] = 0;
+		grid3[2][1][1] = 0;
+		grid3[2][1][2] = 0;
+
+
+		for (int i=0; i<grid1.length; i++) {
+			Pentomino newPentomino = new Pentomino(grid1[i]);
+			pentList.add(newPentomino);
+		}
+		for (int i=0; i<grid2.length; i++) {
+			Pentomino newPentomino = new Pentomino(grid2[i]);
+			pentList.add(newPentomino);
+		}
+		for (int i=0; i<grid3.length; i++) {
+			Pentomino newPentomino = new Pentomino(grid3[i]);
+			pentList.add(newPentomino);
+		}
+		for (int i=0; i<grid4.length; i++) {
+			Pentomino newPentomino = new Pentomino(grid4[i]);
+			pentList.add(newPentomino);
+		}
+		return pentList;
+	}
+
+	/** We rotated and fliped all the pentominoes and stored them in the arrayList */
+	public static ArrayList<ArrayList<Pentomino>> generateVersions(ArrayList<Pentomino> listofPent) {
+		ArrayList<ArrayList<Pentomino>> versions = new ArrayList<ArrayList<Pentomino>>();
+		for (int i=0; i<listofPent.size();i++) {
+			ArrayList<Pentomino> storeRotations = new ArrayList<Pentomino>();
+			Pentomino pents = listofPent.get(i);
+			
+			do {
+				
+				Pentomino clonePento = pents.clone();
+				storeRotations.add(clonePento);
+				Pentomino flipClonePento = pents.clone();
+				flipClonePento.flip();
+				if (!flipClonePento.equals(pents)){
+						storeRotations.add(flipClonePento);
+				}
+				pents.rotate();
+				
+				
+			} while  (storeRotations.size()==0 || !storeRotations.get(0).equals(pents));
+			
+			versions.add(storeRotations);
+		}
+	return versions;
+	} 
+	
+	/**searches matrix for value != 0 => ID*/
+	public static int getIdFromMatrix (MatrixHandler matrix)
+	{
+		int cCol = 0, cRow = 0;
+		while (matrix.getCell (cCol, cRow) == 0 && cCol < matrix.getWidth() && cRow < matrix.getHeight())
+		{
+			cRow++;
+			if (cRow == matrix.getHeight())
+			{
+				cRow = 0;
+				cCol++;
+			}
+		}
+		if (cCol < matrix.getWidth() && cRow < matrix.getHeight())
+			return matrix.getCell (cCol, cRow);
+		else
+			return 0;
+	}
+
+	/** first constructor */
+	public Pentomino(int[][] arPent) {
+		mMatrix = new MatrixHandler(arPent);
+		mId = getIdFromMatrix (mMatrix);
+	}
+
+	/** second constructor */
+	public Pentomino(MatrixHandler matrix) {
+		mMatrix = matrix.clone();
+		mId = getIdFromMatrix (mMatrix);
+	}
+ 
+ 	/** method to clone pentominoes */
+	public Pentomino clone() {
+
+		Pentomino newPentomino = new Pentomino(this.mMatrix);
+			return newPentomino;
+	}
+
+	/** test for flip */
+	public void flip() {
+		mMatrix.flipVer();
+	}
+
+	/** test for rotation */
+	public void rotate() {
+		mMatrix.rotate();
+	}
+	
+	public int getId()
+	{
+		return mId;	
+	}
+
+	public int getWidth() {
+		return mMatrix.getWidth();
+	} 
+
+	public int getHeight() {
+		return mMatrix.getHeight();
+	}
+
+	public int getElement(int x, int y) {
+		return  mMatrix.getCell(x, y);
+
+	}
+	
+	/** Returns the X position of the cell */
+	public int getXCoor(int position) {
+		int height = this.getHeight();
+		int positionX = (int)Math.floor((position-1)/height);
+		return positionX;
+	}
+	
+	/** Returns the Y position of the cell */
+	public int getYCoor(int position) {
+		int height = this.getHeight();
+		int positionY = (int)((position-1)%height);
+		return positionY;
+	}
+
+	/** search for the fisrt used cell starting from the left top corner */
+	public int getLeftTop() {
+		int cntr = 0;
+		for (int i=0; i<getWidth(); i++) {
+			for (int j=0; j<getHeight(); j++) {
+				cntr++;
+				if (mMatrix.getCell(i, j) != 0)
+					return cntr;
+			}
+		}
+				return -1;
+	}
+
+ 	/** find out where the filled cell is located */
+	public int wposition(int cntr) {
+		int w =(int) Math.floor((cntr-1)/getHeight());
+			return w;
+	}
+
+	public int hposition(int cntr) {
+		int h = ((cntr-1)%getHeight());
+			return h;
+	}
+	
+	public boolean equals(Pentomino pent) {
+		return this.mMatrix.equals((pent.mMatrix));
+	}
+	
+	public void debugPrint(){
+		mMatrix.print();
+	}
+	
+	private MatrixHandler mMatrix;
+	private int mId;
+}
+
+
+
+
