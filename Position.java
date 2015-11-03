3a8916d41f594a36aa5984da2f4c8cc2b34eb4a5
Meghun96 <mimwillemsen@gmail.com>
2015-11-03 13:07:20 +0100

    Changes to be committed: 	modified:   Position.java implemented methodes getPosNum that gives you the Position number getDiffX and getDiffY to show the differences between different x and different y addX and addY to add numbers to x and y Position for x and y are found

--- Position.java
+++ Position.java
@@ -1,44 +1,55 @@
-
 public class Position {
-	//Please add comments as you work
+	
 	//Constructor
-	public Position(int posNum, int rows)
-	{
-		
+	/** @param posNum gives position number that indicates a postion in postion number formats
+	* @param rows the number of roms in the board
+	* this constructour looks for the x and y using the rows and posNum**/
+	public Position(int posNum, int rows){
+		x = (int) (posNum/(rows-1));
+		y = (posNum%rows -1)
 	}
 	
-	public Position(int x, int y)
-	{
-		
+	public Position(int x, int y){
+		this.x = x;
+		this.y = y;
 	}
+	
+	/** moddify the x values **/
+	public void addX(int m){
+		x += m;
+	}
+	
+	/** moddify the y values **/
+	public void addY(int n){
+		y += n;
+	}	
 	
 	//getMethods
-	public int getX()
-	{
+	public int getX(){
+		return x;	
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	/** @return the posNum
+	* this gets the postion number using x */
+	public int getPosNum(){
+		return (int) (x*rows) + 1 + y;
 		
 	}
 	
-	public int getY()
-	{
-		
+	/** x differences between two different possitions **/
+	public int getDiffX(Position dx){
+		int diff = this.x - dx.x;
 	}
 	
-	public int getPosNum()
-	{
-		
+	/** y differences between two different possitions **/
+	public int getDiffY(Position dy){
+		int diff = this.y - dy.y;
 	}
 	
-	public int getDiffX()
-	{
-		
-	}
-	
-	public int getDiffY()
-	{
-		
-	}
-	
-	
-	
-	private int x, y;
+	private int x;
+	private int y;
 }
