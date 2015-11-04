d6c7fa25053897d0cc74e4537d2f66cd080f7f3d
MaGa <guser1996.gu@gmail.com>
2015-11-05 00:01:23 +0100

    fixed calculations involving position numbers added missing javadoc comments improved method documentation

--- Position.java
+++ Position.java
@@ -5,47 +5,52 @@ public class Position {
 	* @param rows the number of roms in the board
 	* this constructour looks for the x and y using the rows and posNum**/
 	public Position(int posNum, int rows){
-		x = (int) (posNum/(rows-1));
+		x = Math.ceil (posNum / rows) - 1;
 		y = (posNum%rows -1)
 	}
 	
+	/** @param x x coordinate of the position to be constructed
+	* @param y y coordinate of the position to be constructed**/
 	public Position(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
 	
-	/** moddify the x values **/
+	/** @param m value to be added to x coordinate **/
 	public void addX(int m){
 		x += m;
 	}
 	
-	/** moddify the y values **/
+	/** @param value to be added to y coordinate **/
 	public void addY(int n){
 		y += n;
 	}	
 	
 	//getMethods
+	/** @return x coordinate of position**/
 	public int getX(){
 		return x;	
 	}
 	
+	/** @return y coordinate of position**/
 	public int getY(){
 		return y;
 	}
 	
-	/** @return the posNum
+	/** @param rows number of rows in matrix the position number refers to
+	* @return the posNum
 	* this gets the postion number using x */
-	public int getPosNum(){
-		return (int) (x*rows) + 1 + y;
+	public int getPosNum (int rows){
+		return (int) (x*rows) + y + 1;
 		
 	}
 	
-	/** x differences between two different positions **/
+	/** @return x difference between parameter position and position stored in this object **/
 	public int getDiffX(Position dx){
 		int diff = this.x - dx.x;
 	}
 	
-	/** y differences between two different positions **/
+	/** @return y differences between parameter position and position stored in this object **/
 	public int getDiffY(Position dy){
 		int diff = this.y - dy.y;
 	}
